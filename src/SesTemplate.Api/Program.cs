7417a112d62b28ac439aff7700347ee64dc43828
Wallace Vidal <wallacevff@hotmail.com>
2025-05-16 12:53:03 -0300

    Update Program.cs

--- src/SesTemplate.Api/Program.cs
+++ src/SesTemplate.Api/Program.cs
@@ -1,17 +1,6 @@
 using SesTemplate.Api.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-
+var app = WebApplicationBuilderFactory.CreateWebApplication(args);
 app.UseCors();
 app.AddSwagger();
 app.UseAuthorization();
