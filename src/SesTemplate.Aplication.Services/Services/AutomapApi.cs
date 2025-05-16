516a1af69d7aec2b859c39b222cd10aa3ef6304c
Wallace Vidal <wallacevff@hotmail.com>
2025-05-16 12:26:25 -0300

    Ajuste AutomapApi

---
+++ src/SesTemplate.Aplication.Services/Services/AutomapApi.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+
+namespace SesTemplate.Aplication.Services.Services;
+
+public class AutomapApi(IMapper mapper) : IAutomapApi
+{
+    public TDestination MapFrom<TDestination>(object source)
+        where TDestination : class
+    {
+        return mapper.Map<TDestination>(source);
+    }
+
+
+
+    public void MapTo<TDestination, TSource>(TSource source, TDestination destination)
+        where TDestination : class
+        where TSource : class
+    {
+        mapper.Map(source, destination);
+    }
+
+}
